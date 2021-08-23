599a809616ba40f996a37679f856417714ea6f0c
William Acosta <williammosquera1@outlook.es>
2021-08-23 16:52:19 -0500

    falta intalar el for

--- resources/views/prueba/vehiculo.blade.php
+++ resources/views/prueba/vehiculo.blade.php
@@ -3,5 +3,20 @@
 @section('contenido')
     <a href="prueba/create" class="btn btn-success">Crear vehiculo</a>
 
-    
+    <table class="table table-dark table-striped mt-4">
+        <thead>
+            <tr>
+                <th scope="col">Placa</th>
+                <th scope="col">Color</th>
+                <th scope="col">Marca</th>
+                <th scope="col">Tipo de vehículo</th>
+                <th scope="col">Conductor</th>
+                <th scope="col">Propietario</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+
+    </table>
 @endsection
