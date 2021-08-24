771a6d88024158b1e7eb46231f64780aa7eb578e
57300 <WilliamMosquera1@outlook.es>
2021-08-23 22:00:21 -0500

    tablas corregidas

---
+++ database/migrations/2021_08_24_015005_create_propietarios_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropietariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('propietarios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('veiculo_id');
+            $table->foreign('veiculo_id')->references('id')->on('vehiculos');
+            $table->char('documento',12);
+            $table->string('p_nombre',7);
+            $table->string('s_nombre',7);
+            $table->string('apellidos',14);
+            $table->string('direccion',20);
+            $table->char('telefono',10);
+            $table->string('ciudad',15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('propietarios');
+    }
+}
